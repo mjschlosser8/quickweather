7911c8a0ef3440142ddd064160a393192309d411
Matt Schlosser <mschlosser87@gmail.com>
2019-02-02 13:40:47 -0500

    Began five day forecast work

--- weatherApp.rb
+++ weatherApp.rb
@@ -36,3 +36,14 @@ weatherdesc = JSON.parse(response)["weather"][0]["description"]
 puts "Conditions: " + weathercon
 puts "That " + weathercon + " is " + weatherdesc
 puts "____________________"
+
+#Five day forecast
+puts "Here's your five-day forecast:"
+five_day_url = "https://api.openweathermap.org/data/2.5/forecast?zip=" + "#{zip}" + "&units=imperial&APPID=486516e4f171e6170947f80ba3e533f9"
+five_day_encoded_url = URI.encode(five_day_url)
+five_day_response = HTTParty.get(five_day_encoded_url).to_s
+forecast = JSON.parse(five_day_response)
+puts forecast["list"]["weather"]['main']
+#forecast["list"]["weather"].each do
+#  print ["main"]
+#end
